7ec23c331a848828609be93297542cb271d33587
SteveBattista <SteveBattista>
2024-10-12 09:40:30 +0000

    added verbose funtion

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Implements hashing with a configurable thread pool<br>
     -a, --algorithm <256 | 384 | 512>    Chooses what algorthim to use SHA256->(256), SHA384->(384) or SHA512->(512),
                                          Default is SHA256. <br> <br>
     -p, --pool <#>                       Sets the size of the pool of maximum number of concurrent threads when hashing.<br>
-                                         Default is 10. Large numbers (> 60) may cause the progam not to hash all files.<br>
+                                         Default is mumber of CPUs. Large numbers (> 60) may cause the progam not to hash all files.<br>
 
 <h3>ARGS:<br></h3>
     <files>...    Place one or more files to hash. Those that can not be found will be ommited from the results.<br>
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,122 @@
 #![forbid(unsafe_code)]
 use clap::{App, Arg};
 use data_encoding::HEXUPPER;
-use ring::digest::{Context, SHA256, SHA384, SHA512,Algorithm,Digest};
+use indicatif::MultiProgress;
+use indicatif::ProgressBar;
+use indicatif::ProgressStyle;
+use ring::digest::{Algorithm, Context, Digest, SHA256, SHA384, SHA512};
 use scoped_threadpool::Pool;
 use std::error::Error;
 use std::fs;
 use std::io::{BufReader, Read};
 use std::process::exit;
+//use std::sync::Arc;
+use std::time::Duration;
+use std::time::UNIX_EPOCH;
+use walkdir::WalkDir;
+const FILENAME_LEN: usize = 120;
 
 fn main() {
     let matches = App::new("Threaded Hasher")
-                          .version("1.0")
-                          .author("Stephen Battista <stephen.battista@gmail.com>")
-                          .about("Implements hashing with a configurable thread pool")
-                          .arg(Arg::with_name("algo")
+                            .version("1.0")
+                            .author("Stephen Battista <stephen.battista@gmail.com>")
+                            .about("Implements hashing with a configurable thread pool")
+                            .arg(Arg::with_name("algo")
                                .short("a")
                                .long("algorithm")
                                .value_name("256 | 384 | 512")
-                               .help("Chooses what algorthim to use SHA256->(256), SHA384->(384) or SHA512->(512). Default is SHA256.")
+                               .help("Chooses what algorthim to use SHA256->(256), SHA384->(384) or SHA512->(512). Default is SHA512.")
                                .takes_value(true))
-                          .arg(Arg::with_name("pool")
-			        .short("p")
+                            .arg(Arg::with_name("verbose")
+                               .short("v")
+                              .long("verbose")
+                              .help("Shows the names of the files being hashed.")
+                              .takes_value(false))
+                            .arg(Arg::with_name("pool")
+			                    .short("p")
                                .long("pool")
                                .value_name("#")
-                               .help("Sets the size of the pool of maximum number of concurrent threads when hashing. Default is 10. Large numbers (> 60) may cause the progam not to hash all files.")
+                               .help("Sets the size of the pool of maximum number of concurrent threads when hashing. Default is number of CPUs. Large numbers (> 60) may cause the progam not to hash all files.")
                                .takes_value(true))
-                          .arg(Arg::with_name("files")
-                               .value_name("files")
-                               .help("Place one or more files to hash. Those that can not be found will be ommited from the results. Directories will be ommitted. Links will be treated like normal files.")
-                               .required(true).min_values(1))
-                          .get_matches();
+                            .arg(Arg::with_name("directory")
+                               .short("d")
+                               .long("directory")
+                               .value_name("DIRECTORY")
+                               .help("Directory to start hashing. Default is current working directory. Program does not follow symbolic links.")
+                               .takes_value(true))
+                            .get_matches();
 
-    let hashalgo: &Algorithm;
-    let inputhash = matches.value_of("algo").unwrap_or("256");
-    match inputhash {
-        "256" => hashalgo = &SHA256,
-        "384" => hashalgo = &SHA384,
-        "512" => hashalgo = &SHA512,
+    let inputhash = matches.value_of("algo").unwrap_or("512");
+    let verbose = matches.is_present("verbose");
+    let hashalgo: &Algorithm = match inputhash {
+        "256" => &SHA256,
+        "384" => &SHA384,
+        "512" => &SHA512,
         _ => {
             println!("Please choose 256, 384 or 512 for type of SHA hash.");
             exit(0);
         }
-    }
+    };
     //  println!("Hash chosen is {}", inputhash);
-
-    let inputpool = matches.value_of("pool").unwrap_or("10");
-    let poolresult = inputpool.parse();
-    let poolnumber;
-    match poolresult {
-        Ok(n) => poolnumber = n,
+    let binding = num_cpus::get().to_string();
+    let inputpool = matches.value_of("pool").unwrap_or(&binding);
+    let poolresult = &inputpool.parse();
+    let poolnumber = match poolresult {
+        Ok(n) => n,
         Err(_e) => {
             println!("Please choose a number for the number of threads.");
             exit(0);
         }
+    };
+    let mut pool = Pool::new(*poolnumber);
+    // println!("pool chosen is {}", inputpool);
+    // println!("Files chosen is {}", inputpool.len());
+
+    let input_directoy = matches.value_of("directory").unwrap_or(".");
+
+    let mut inputfiles: Vec<String> = Vec::new();
+    let spinner = ProgressBar::new_spinner();
+    spinner.set_prefix("Constucting file list:");
+    spinner.set_style(
+        ProgressStyle::default_bar()
+            .template("{prefix} [{elapsed_precise}] {spinner:.yellow/cyan}")
+            .expect("Style is wrong."),
+    );
+    for entry in WalkDir::new(input_directoy) {
+        inputfiles.push(entry.unwrap().path().display().to_string());
+
+        spinner.tick();
     }
-    let mut pool = Pool::new(poolnumber);
-    //    println!("pool chosen is {}", inputpool);
 
-    let inputfiles: Vec<_> = matches.values_of("files").unwrap().collect();
-    //   println!("Files chosen is {}", inputpool.len());
+    spinner.finish();
+    let m = MultiProgress::new();
+    let bar = ProgressBar::new(inputfiles.len() as u64);
+    let pb = m.add(bar);
 
+    //pb.set_style(sty.clone());
     pool.scoped(|scoped| {
-        for file in inputfiles {
+        for mut file in inputfiles {
+            let pb_clone = pb.clone();
+            let m = m.clone();
             scoped.execute(move || {
-                let _x = gethashofile(&file, hashalgo);
+                if verbose {
+                    if file.len() > FILENAME_LEN {
+                        let split_pos = file.char_indices().nth_back(FILENAME_LEN).unwrap().0;
+                        file = file[split_pos..].to_string();
+                    }
+                    let spinner = m.add(ProgressBar::new_spinner().with_message(file.clone()));
+                    spinner.enable_steady_tick(Duration::from_millis(100));
+                    let _x = gethashofile(&file, hashalgo);
+                    pb_clone.inc(1);
+                    spinner.finish_and_clear();
+                } else {
+                    let _x = gethashofile(&file, hashalgo);
+                    pb_clone.inc(1);
+                }
             });
         }
     });
+    spinner.finish_and_clear();
 }
 
 fn var_digest<R: Read>(
@@ -91,7 +141,18 @@ fn gethashofile(path: &str, hashalgo: &'static Algorithm) -> Result<(), Box<dyn 
     let input = fs::File::open(path)?;
     let reader = BufReader::new(input);
     let digest = var_digest(reader, hashalgo)?;
-    println!("{} : {} : {}", path, metadata.len(), HEXUPPER.encode(digest.as_ref()));
+    println!(
+        "{} : {} : {} : {}",
+        path,
+        metadata.len(),
+        metadata
+            .modified()
+            .unwrap()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs(),
+        HEXUPPER.encode(digest.as_ref())
+    );
     Ok(())
 }
 
@@ -112,9 +173,8 @@ mod tests {
     fn test_from_hex() {
         let acutal = "DEADBEEF";
         let acutal_binary = HEXUPPER.decode(b"DEADBEEF").unwrap();
-        assert_eq!(test::from_hex(acutal).unwrap(),acutal_binary);
+        assert_eq!(test::from_hex(acutal).unwrap(), acutal_binary);
     }
-
 
     #[test]
     fn test_256_1() {
@@ -175,5 +235,4 @@ mod tests {
         let expected: Vec<u8> = test::from_hex(expected_hex).unwrap();
         assert_eq!(&expected, &actual.as_ref());
     }
-
 }
